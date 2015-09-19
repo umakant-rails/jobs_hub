bb98bf1afe8e15c3cb8beaa61f874f73da0bc5aa
umakant-rails <rajpootumakant@gmail.com>
2015-09-19 16:41:14 +0530

    add copyright text and some css

--- app/helpers/admin/job_posts_helper.rb
+++ app/helpers/admin/job_posts_helper.rb
@@ -4,14 +4,15 @@ module Admin::JobPostsHelper
     job_posts = @job.job_posts
     job_post_str = ''
 
-    if job_posts.map(&:qualifications).uniq.size == 1
+    if job_posts && job_posts.map(&:qualifications).uniq.size == 1
       return job_posts.map(&:qualifications)[0]
-    else
+    elsif job_posts.present?
       job_posts.each do | job_post |
         job_post_str = job_post_str + "<strong>" + job_post.post_name + " </strong>" + job_post.qualifications + "<br/>"
       end
       return job_post_str.html_safe
+    else
+      return 'Please add job posts for this Job.'
     end
-
   end
 end
