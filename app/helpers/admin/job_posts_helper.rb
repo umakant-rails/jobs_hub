c2cda2152c856258deada06449cb37307678648a
umakant-rails <rajpootumakant@gmail.com>
2015-09-19 15:29:31 +0530

    add a helper method for job post qualification string

--- app/helpers/admin/job_posts_helper.rb
+++ app/helpers/admin/job_posts_helper.rb
@@ -1,2 +1,17 @@
 module Admin::JobPostsHelper
+
+  def get_job_qualification
+    job_posts = @job.job_posts
+    job_post_str = ''
+
+    if job_posts.map(&:qualifications).uniq.size == 1
+      return job_posts.map(&:qualifications)[0]
+    else
+      job_posts.each do | job_post |
+        job_post_str = job_post_str + "<strong>" + job_post.post_name + " </strong>" + job_post.qualifications + "<br/>"
+      end
+      return job_post_str.html_safe
+    end
+
+  end
 end
