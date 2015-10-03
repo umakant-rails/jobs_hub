d0a78320df9b86b30fbe00d8c608aae52638e3ec
umakant-rails <rajpootumakant@gmail.com>
2015-10-04 02:41:11 +0530

    add jobs and states pop up with job listing style

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,4 +10,44 @@ $(document).ready(function(){
     $("#remaining_states").css({display: 'block'});
     $(this).hide();
   });
+
+  /* Open states list on hover event when medius and large size (pc etc ) devise is used */
+  $(".state-list-tab, .states-name-list, .states-name-block").hover(function(){
+    if($("#fa-menubar-button").css('display') == "none"){
+      $(".states-name-list").css({display: 'block'});
+    }
+  },function(){
+    if($("#fa-menubar-button").css('display') == "none"){
+      $(".states-name-list").css({display: 'none'});
+    }
+  });
+
+  /* Open states list on click event when small size (pc etc ) devise is used */
+  $(".state-list-tab").on('click', function(){
+    var menuBarBtnStatus = $("#fa-menubar-button").css('display');
+    var menuListOpenStatus = $(".states-name-list").css('display');
+
+    if(menuBarBtnStatus == "block" && menuListOpenStatus == 'none'){
+      $(".states-name-list").css({display: 'block'});
+    }else if(menuBarBtnStatus == "block"){
+      $(".states-name-list").css({display: 'none'});
+    }
+  });
+
+  /*$(".breakhead, .breaking-news-list").hover(function(){
+    $(".breaking-news-list").css({display: 'block'});
+  },function(){
+    $(".breaking-news-list").css({display: 'none'});
+  });*/
+  $(".breakhead").on('click', function(){
+    if($(".breakhead i").hasClass('glyphicon-plus')){
+      $(".breakhead i").removeClass('glyphicon-plus');
+      $(".breakhead i").addClass('glyphicon-minus');
+      $(".breaking-news-list").css({display: 'block'});
+    }else {
+      $(".breakhead i").addClass('glyphicon-plus');
+      $(".breakhead i").removeClass('glyphicon-minus');
+      $(".breaking-news-list").css({display: 'none'});
+    }
+  });
 });
--- app/models/announcement.rb
+++ app/models/announcement.rb
@@ -8,7 +8,7 @@ class Announcement < ActiveRecord::Base
   TITLE_NAMES = {
     "1": "Upcoming Notifications",
     "2": "Admit Card / Call Letter",
-    "3": "Inteviewer / Exam Date Reminder",
+    "3": "Inteview / Exam Date Reminder",
     "4": "Interview/Exam Results"
   }
 end
